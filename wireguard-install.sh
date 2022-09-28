c1f163326602b0a7c1968024a2a36c50780a0b11
Levon Aghatelyan <l@laa.am>
2022-09-28 16:12:33 +0400

    Don't prompt for an IP address if it's defined correctly

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -74,14 +74,14 @@ function installQuestions() {
 	echo ""
 
 	# Detect public IPv4 or IPv6 address and pre-fill for the user
-	SERVER_PUB_IP=$(ip -4 addr | sed -ne 's|^.* inet \([^/]*\)/.* scope global.*$|\1|p' | awk '{print $1}' | head -1)
-	if [[ -z ${SERVER_PUB_IP} ]]; then
+	SERVER_IP=$(ip -4 addr | sed -ne 's|^.* inet \([^/]*\)/.* scope global.*$|\1|p' | awk '{print $1}' | head -1)
+	if [[ -z ${SERVER_IP} ]]; then
 		# Detect public IPv6 address
-		SERVER_PUB_IP=$(ip -6 addr | sed -ne 's|^.* inet6 \([^/]*\)/.* scope global.*$|\1|p' | head -1)
+		SERVER_IP=$(ip -6 addr | sed -ne 's|^.* inet6 \([^/]*\)/.* scope global.*$|\1|p' | head -1)
 	fi
 	
 	until [[ ${SERVER_PUB_IP} =~ ^([0-9]{1,3}\.){3} ||  ${SERVER_PUB_IP} =~ ^([a-f0-9]{1,4}:){3,4}: ]]; do
-		read -rp "IPv4 or IPv6 public address: " -e -i "${SERVER_PUB_IP}" SERVER_PUB_IP
+		read -rp "IPv4 or IPv6 public address: " -e -i "${SERVER_IP}" SERVER_PUB_IP
 	done
 	
 	# Detect public interface and pre-fill for the user
